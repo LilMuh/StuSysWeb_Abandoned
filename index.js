53d13aaba3efae76ccacb8b9e6c36ec7e5e00369
Tom Huang <99058458+LilMuh@users.noreply.github.com>
2023-11-02 00:50:55 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -3,24 +3,51 @@ import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from "@/views/LoginView.vue";
 import AdminView from "@/views/AdminView.vue";
+import LayoutView from "@/views/Layout.vue";
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
+
   {
     path: '/login',
     name: 'login',
     component: LoginView
   },
   {
-    path: '/admin',
-    name: 'about',
-    component: AdminView
+    path: '/',
+    name: 'Layout',
+    component: LayoutView,
+    children:[
+      {
+        path: '',
+        name: 'home',
+        component: HomeView
+      },
+      {
+        path: 'admin',
+        name: 'about',
+        component: AdminView
+      },
+      {
+        // havent done
+        path: 'user',
+        name: 'home',
+        component: HomeView
+      },
+      {
+        // havent done
+        path: 'StudentInfo',
+        name: '',
+        component: HomeView
+      },
+      {
+        // havent done
+        path: 'StaffInfo',
+        name: '',
+        component: HomeView
+      }
+    ]
   }
 ]
 
